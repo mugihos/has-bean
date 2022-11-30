dfa93de7620dfd4334c2b7a4d7c215bc6b04e6dc
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-11-30 22:00:51 +1300

    add 404 page with random coffee image API, and add _theme.scss

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const server = express()
+const request = require('superagent')
 
 const roastersRoutes = require('./routes/roasters')
 
@@ -8,6 +9,18 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/roasters', roastersRoutes)
+
+server.get('/api/v1/coffeeimage', (req, res) => {
+  request
+  .get(`https://coffee.alexflipnote.dev/random.json`)
+  .then((response) => {
+    res.json(response.body)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.sendStatus(500)
+  })
+})
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
