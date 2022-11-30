bf359216802a4a8e06b3ed13295f47f0ca5e12fc
Clinton Evans <clintonevans@Biscuit.local>
2022-11-30 17:24:28 +1300

    create migrations for our database

---
+++ server/db/migrations/20221130041358_create_roasteries_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('roasteries', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('location')
+    table.string('detail')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('roasteries')
+}
