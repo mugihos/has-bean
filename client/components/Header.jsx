9c4e386237be2ec8fbfe77448ad64721086ec4f6
Sarah-Dewbery <111667349+Sarah-Dewbery@users.noreply.github.com>
2022-12-01 16:07:32 +1300

    Links added to header and working

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Header.module.scss'
 
+
+// import { IfAuthenticated, IfNotAuthenticated } from './isAuthenticated'
+// import { useAuth0 } from '@auth0/auth0-react'
 
 export default function Header() {
+  // const { logout, loginWithRedirect, user } = useAuth0()
+
+  // const handleLogOff = (e) => {
+  //   e.preventDefault()
+  //   logout()
+  // }
+
+  // const handleSignIn = (e) => {
+  //   e.preventDefault()
+  //   loginWithRedirect()
 
   return (
     <>
+    <nav className={styles.navbar}>
+      
       <div>
-        <h1>HAS BEAn</h1>
+        <Link to='/'>
+        <h1 className={styles.title}>HAS BEAN</h1>
+        </Link>
       </div>
+      <div className={styles.links}>
+        <Link to='/roasters'>Roasters</Link>
+        <Link to='/beans'>Beans</Link>
+        <Link to='/ivebeen'>I've Been</Link>
+        </div>
+        {/* <div>
+        <IfAuthenticated>
+          <div className={styles.auth}>
+            <div>Hello {user?.nickname}</div>
+            <div>
+              <Link to='/' onClick={handleLogOff}>
+                Sign out
+              </Link>
+            </div>
+          </div>
+        </IfAuthenticated>
+
+        <IfNotAuthenticated>
+          <Link className={styles.auth} to='/' onClick={handleSignIn}>
+            Register | Login
+          </Link>
+        </IfNotAuthenticated>
+      </div> */}
+
+    </nav>
     </>
   )
 }
