037dee7f22cdd1c4670e912746cd2eeafd9a973e
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-05 10:41:43 +1300

    update test for roatsers add

--- client/components/AddRoaster.jsx
+++ client/components/AddRoaster.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { submitRoaster } from '../actions/roasters'
-// import styles from './AddRoaster.module.scss'
+import styles from './AddRoaster.module.scss'
 
 export default function AddRoaster() {
   const dispatch = useDispatch()
