0884866a2488f6b2f07dc14049c4ef53d06e6e3b
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-04 14:11:50 +1300

    add update/del/add funtion for roaster

--- server/db/roasters.js
+++ server/db/roasters.js
@@ -1,8 +1,23 @@
 const connection = require('./connection')
 module.exports = {
   getRoasters,
+  addRoaster,
+  editRoaster,
+  deleteRoaster,
 }
 
 function getRoasters(db = connection) {
   return db('roasters').select()
+}
+
+function addRoaster(roaster, db = connection) {
+  return db('roasters').insert(roaster)
+}
+
+function editRoaster(id, newInfo, db = connection) {
+  return db('roasters').where({ id }).update(newInfo)
+}
+
+function deleteRoaster(id, db = connection) {
+  return db('roasters').where({ id }).del()
 }
