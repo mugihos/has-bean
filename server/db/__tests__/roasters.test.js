7ae031bb5574e5e2a752523894ee3da2587c99fd
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-05 11:26:34 +1300

    fix merge conflit for db test roasters

--- server/db/__tests__/roasters.test.js
+++ server/db/__tests__/roasters.test.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const testConfig = require('../knexfile').test
 const testDb = knex(testConfig)
 
-const { getRoasters } = require('../roasters')
+const { getRoasters, addRoaster } = require('../roasters')
 
 beforeAll(() => {
   return testDb.migrate.latest()
@@ -18,51 +18,23 @@ afterAll(() => {
 
 describe('get roasters', () => {
   it('get all roasters', () => {
+    expect.assertions(4)
     return getRoasters(testDb).then((roasters) => {
-      //console.log(cafes)
       expect(roasters).toHaveLength(roasters.length)
       expect(roasters[0].name).toBe('Coffee Supreme')
       expect(roasters[0].location).toBe('Wellington, Auckland, Christchurch')
       expect(roasters[2].name).toBe('Kōkako Organic Coffee')
     })
-  }),
-    it('test for Null names in roasters', () => {
-      return getRoasters(testDb).then((roasters) => {
-        //console.log(cafes)
-        expect(roasters).not.toBeNull()
-      })
-    })
-})
-const knex = require('knex')
-const { test } = require('../knexfile')
-const testConfig = require('../knexfile').test
-const testDb = knex(testConfig)
-const { getRoasters, addRoaster } = require('../roasters')
-
-beforeAll(() => {
-  return testDb.migrate.latest()
-})
-
-beforeEach(() => {
-  return testDb.seed.run()
-})
-
-afterAll(() => {
-  return testDb.destroy()
-})
-
-describe('getRosters', () => {
-  it('gets all roasters from roaster table in db', () => {
-    expect.assertions(2)
-    return getRoasters(testDb).then((roaster) => {
-      expect(roaster).toHaveLength(20)
-      expect(roaster[0].name).toBe('Coffee Supreme')
+  })
+  it('test for Null names in roasters', () => {
+    expect.assertions(1)
+    return getRoasters(testDb).then((roasters) => {
+      expect(roasters).not.toBeNull()
     })
   })
   it('add a new roaster to the roatser table in db', () => {
     expect.assertions(1)
     const fakeData = {
-      id: 1,
       name: 'Coffee Test',
       location: 'Wellington',
       details: 'Better coffee for all',
