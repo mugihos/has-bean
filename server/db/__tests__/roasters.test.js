15456b8685bd894419d76435194d8ae48126025d
Clinton Evans <112045187+clinton-b-evans@users.noreply.github.com>
2022-12-05 12:20:37 +1300

    resolve URL NULL field test plus seed was missing field

--- server/db/__tests__/roasters.test.js
+++ server/db/__tests__/roasters.test.js
@@ -29,7 +29,10 @@ describe('get roasters', () => {
     it('test for Null names in roasters', () => {
       return getRoasters(testDb).then((roasters) => {
         //console.log(cafes)
-        expect(roasters).not.toEqual(null)
+        expect(roasters).not.toBeNull()
+        roasters.map((roaster) => {
+          expect(roaster.url).not.toBeNull()
+        })
       })
     })
 })
