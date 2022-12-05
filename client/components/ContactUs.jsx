9da35219ba6369a0744e9f3054be7066a385ac56
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-12-06 12:29:33 +1300

    Add working button for contact us

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState }  from 'react'
 import styles from './ContactUs.module.scss'
 
 export default function ContactUs() {
+  const [sendToggle, setSendToggle] = useState(false)
+
+  function handleOnClick() {
+    setSendToggle(true)
+  }
 
 
   return (
     <>
     <h2>Get in touch</h2>
     <h3>Drop us a line</h3>
-    <p>Email us with any questions you have and we'll get back to you as soon as we can.</p>
+    <p>Email us with any questions you have and we&apos;ll get back to you as soon as we can.</p>
     <button>aihe2022.coffee@gmail.com</button>
     <p>Alternatively you can fill out this form</p>
 
     <h3>Contact form</h3>
-    <input placeholder='First Name*'></input><p></p>
+    { sendToggle == true ? <div><p>Thank you! We&apos;ll get back to you as soon as we can.</p></div> : 
+    <div><input placeholder='First Name*'></input><p></p>
     <input placeholder='Last Name*'></input><p></p>
     <input placeholder='Email Address*'></input><p></p>
     <input placeholder='Subject*'></input><p></p>
     <input placeholder='Your phone number'></input><p></p>
     <textarea placeholder='Your message'></textarea><p></p>
-    <button>Send message</button>
+    <button onClick={handleOnClick}>Send message</button>
+    </div> }
     </>
   )
 }
