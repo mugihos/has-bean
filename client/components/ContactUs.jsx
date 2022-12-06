dbc3d41189f2be3b5132b1b615ff191482fc381d
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-12-06 13:01:04 +1300

    Update contact us page with mailto

--- client/components/ContactUs.jsx
+++ client/components/ContactUs.jsx
@@ -14,12 +14,17 @@ export default function ContactUs() {
     <h2>Get in touch</h2>
     <h3>Drop us a line</h3>
     <p>Email us with any questions you have and we&apos;ll get back to you as soon as we can.</p>
-    <button>aihe2022.coffee@gmail.com</button>
-    <p>Alternatively you can fill out this form</p>
+    <button>
+      <a href="mailto:aihe2022.coffee@gmail.com?subject=Query from HasBean">
+        aihe2022.coffee@gmail.com
+      </a>
+    </button>
+    <p>Alternatively you can fill out the form below.</p>
 
     <h3>Contact form</h3>
-    { sendToggle == true ? <div><p>Thank you! We&apos;ll get back to you as soon as we can.</p></div> : 
-    <div><input placeholder='First Name*'></input><p></p>
+    { sendToggle == true ? <div><p>Thank you! We&apos;ll get back to you as soon as possible.</p></div> : 
+    <div>
+    <input placeholder='First Name*'></input><p></p>
     <input placeholder='Last Name*'></input><p></p>
     <input placeholder='Email Address*'></input><p></p>
     <input placeholder='Subject*'></input><p></p>
