af385512c980d3ca0668e95ab058551cab1344cf
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-12-07 01:17:49 +1300

    Update bits and bobs

--- client/components/OurStory.jsx
+++ client/components/OurStory.jsx
@@ -7,7 +7,7 @@ export default function OurStory() {
   return (
     <>
     <h2 className={styles.branchFont}>Our Story</h2>
-    <h4>We&apos;re a team of 6 passionate junior devs on a coffee journey.</h4>
+    <h4>Have you ever wanted a place to store your coffee experiences?</h4>
     </>
   )
 }
