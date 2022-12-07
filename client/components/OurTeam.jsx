c911a59240aad41f7886873131c85a369b8f2b0e
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-07 16:32:27 +1300

    add contact form, team and story page

--- client/components/OurTeam.jsx
+++ client/components/OurTeam.jsx
@@ -7,8 +7,58 @@ export default function OurTeam() {
       <h2 className={styles.branch}>Our Team</h2>
       <div className={styles.container}>
         <div className={styles.individual}>
-          <h4>NAME</h4>
+          <h4 className={styles.name}>Clinton</h4>
           <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className={styles.individual}>
+          <h4 className={styles.name}>Eq</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className={styles.individual}>
+          <h4 className={styles.name}>Gus</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className={styles.individual}>
+          <h4 className={styles.name}>Ingrid</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className={styles.individual}>
+          <h4 className={styles.name}>Mugiho</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
+        </div>
+        <div className={styles.individual}>
+          <h4 className={styles.name}>Sarah</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+          <ul className={styles.profile}>
+            <li className={styles.list}>Hello hello</li>
+            <li>GitHub:</li>
+            <li></li>
+          </ul>
         </div>
       </div>
     </>
