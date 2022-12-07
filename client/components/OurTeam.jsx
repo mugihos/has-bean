1cbdbf28e8a5f2d7d133ab2840ff997904fe827c
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-07 14:22:34 +1300

    adding image

--- client/components/OurTeam.jsx
+++ client/components/OurTeam.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import styles from './OurTeam.module.scss'
 
 export default function OurTeam() {
-
-
   return (
     <>
-    <h2 className={styles.branchFont}>Our Team</h2>
-    <h4>Meet us! We are...</h4>
-    <h2>Mugi, Gus, Ingrid, Clinton, Sarah and Eq</h2>
+      <h2 className={styles.branch}>Our Team</h2>
+      <div className={styles.container}>
+        <div className={styles.individual}>
+          <h4>NAME</h4>
+          <img className={styles.photo} src="/img/bean.png" alt="" />
+        </div>
+      </div>
     </>
   )
 }
-
+{
+  /* <h3>
+Meet us! We are 6 developers passionate about creating a platform that{' '}
+</h3> */
+}
